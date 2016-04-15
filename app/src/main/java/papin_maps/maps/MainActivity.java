52ae176409dd5e07c438a647bf06a14fb0b90ae7
Papin <papironi1>
2016-04-15 19:43:38 +0300

    -vector dosen't works with marker

--- app/src/main/java/papin_maps/maps/MainActivity.java
+++ app/src/main/java/papin_maps/maps/MainActivity.java
@@ -1,13 +1,37 @@
 package papin_maps.maps;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap map;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap)
+    {
+        map = googleMap;
+        BitmapDescriptor factory = BitmapDescriptorFactory.fromResource(R.drawable.powered_by_google_light);
+        LatLng myPosition = new LatLng(-34, 151);
+        map.addMarker(new MarkerOptions().position(myPosition).title("Me").draggable(true).icon(factory));
+        map.moveCamera(CameraUpdateFactory.newLatLng(myPosition));
     }
 }
